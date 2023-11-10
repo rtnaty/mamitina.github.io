cc284fcbb9b5789230a90288936db0c624e2cb0c
Mamitina <tsilavina.ramanatsoa@techinnov.ca>
2023-11-10 09:35:05 -0500

    update portolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import i18n from './i18n';
 import Header from './components/Header';
 import About from './components/About';
@@ -9,14 +9,25 @@ function App() {
   const switchLanguage = () => {
     const newLang = i18n.language === 'fr' ? 'en' : 'fr';
     i18n.changeLanguage(newLang);
+    localStorage.setItem('preferredLanguage', newLang);
   };
+
+  // Set the initial language based on the stored preference or default to 'fr'
+  useEffect(() => {
+    const storedLang = localStorage.getItem('preferredLanguage');
+    if (storedLang) {
+      i18n.changeLanguage(storedLang);
+    } else {
+      i18n.changeLanguage('fr');
+    }
+  }, []);
 
   return (
     <div>
-      <Header switchLanguage={switchLanguage}/>
-      <About/>
-      <Resume/>
-      <Footer/>
+      <Header switchLanguage={switchLanguage} />
+      <About />
+      <Resume />
+      <Footer />
     </div>
   );
 }
