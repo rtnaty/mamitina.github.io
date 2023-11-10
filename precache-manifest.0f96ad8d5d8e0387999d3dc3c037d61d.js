4e3433c89cbbc6f94696fddfb77300cacac8b532
Mamitina <tsilavina.ramanatsoa@techinnov.ca>
2023-11-10 15:56:42 -0500

    Updates

---
+++ precache-manifest.0f96ad8d5d8e0387999d3dc3c037d61d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752d7c8016c1ee24384599187594da23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0cfe8fddb082d700a84",
+    "url": "/static/js/2.425b0811.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.425b0811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52eb43e3bb1cb0031332",
+    "url": "/static/js/main.6199ee9f.chunk.js"
+  },
+  {
+    "revision": "d72652f21cfe9175ef86",
+    "url": "/static/js/runtime-main.09f294bd.js"
+  }
+]);
